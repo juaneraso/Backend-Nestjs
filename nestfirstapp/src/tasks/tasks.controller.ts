3bd06be8ec87c0bb6ae60fccabc2dc1b12e8b63f
Juan <juangui9508@hotmail.com>
2024-01-15 09:21:28 -0500

    get-post

--- nestfirstapp/src/tasks/tasks.controller.ts
+++ nestfirstapp/src/tasks/tasks.controller.ts
@@ -1,11 +1,25 @@
-import { Controller,Get } from '@nestjs/common';
+import { Body, Controller,Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/task.dto';
+
 
 @Controller('tasks')   //decorador
 export class TasksController {
+
+  constructor( private tasksService : TasksService){
+
+  }
  
   @Get()  
-  helloworld(){
-   return "hello world";
+  getAlltasks(){
+  //  return "hello world";
+   return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() newTask:CreateTaskDto){
+   
+    return this.tasksService.createTask(newTask.title,newTask.description);
   }
 
 
